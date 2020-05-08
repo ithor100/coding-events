8671e1feea85e30357c34de5ef15eb82a89042bc
Inga Thor <mail@ingathor.com>
2020-05-08 11:51:01 +0200

    Create input form scaffold for Create Event Page

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Create an Event</h1>
+    <form>
+      <input type="text" name="title" placeholder="Add a title" id="" />
+      <input type="submit" value="Create Event" />
+    </form>
   </div>
 </template>
 
--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -4,16 +4,38 @@ import CreateEvent from "@/views/CreateEvent";
 describe("CreateEvent", () => {
   test("it has an h1 heading", () => {
     const wrapper = mount(CreateEvent);
-
     // expect that there is an h1
     // carets <> not needed
-    // also works for "h1, h2" meaning check for h1 OR h2
+    // also works for "h1, h2" meaning check either h1 OR h2
     expect(wrapper.contains("h1")).toBe(true);
   });
 
   // it should have a heading saying Create an Event
-  test("it has a heading saying Create an Event text content", () => {
+  test("has a heading saying Create an Event text content", () => {
     const wrapper = mount(CreateEvent);
     expect(wrapper.get("h1").text()).toBe("Create an Event");
+  });
+
+  test("contains a form element", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.contains("form")).toBe(true);
+  });
+
+  test("contains a title input field", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.contains("input[type='text'][name='title']")).toBe(true);
+  });
+
+  test("contains a submit button with value Create", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.contains("input[value='Create Event'][type='submit']")).toBe(
+      true
+    );
+  });
+
+  test("contains an input field for the title with the placefolder 'Add a title'", () => {
+    const wrapper = mount(CreateEvent);
+    const titleInput = wrapper.get("input[name='title']");
+    expect(titleInput.attributes("placeholder")).toBe("Add a title");
   });
 });
